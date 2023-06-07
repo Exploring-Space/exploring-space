78ec70f803dd8adbcf075748be1364fd564f1fb8
Jonathan Zeray <jonathan.zeray@hotmail.com>
2023-06-07 14:52:23 +0200

    new audio that only plays once,

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,20 +3,27 @@ import { LandingPageText } from "./canvas/3dText";
 import { Canvas } from "@react-three/fiber";
 import { Loader } from "@react-three/drei";
 import { useNavigate } from "react-router-dom";
-import launchAudio from "/assets/Audio/launchAudio.mp3";
+import launchAudio from "/assets/Audio/armstrongSmallStepCrop.wav";
 import { Background } from "./canvas/Background";
+import { PresentationControls } from "@react-three/drei";
+import { useState } from "react";
 
 export function LandingPage() {
+  const [clicked, setClicked ] = useState(false);
+
   const navigate = useNavigate();
 
   function startAudio() {
-    new Audio(launchAudio).play();
+    if (!clicked) {
+      setClicked(true)
+      new Audio(launchAudio).play();
+    }
   }
 
   function handleClick() {
     setTimeout(() => {
       navigate("/Explore");
-    }, 3000);
+    }, 4000);
   }
 
   return (
@@ -26,9 +33,11 @@ export function LandingPage() {
         {/* <pointLight position={[0, 20, 10]} intensity={1.5}/> */}
         <ambientLight intensity={0.1} />
         <directionalLight color="#e7eaea" position={[0, 0, 5]} />
+        <PresentationControls snap>
         <Float speed={0.8}>
           <LandingPageText />
         </Float>
+        </PresentationControls>
       </Canvas>
       <Loader />
       <button
@@ -36,9 +45,9 @@ export function LandingPage() {
           handleClick();
           startAudio();
         }}
-        className="absolute justify-self-center bottom-1/4 z-1 w-60 p-5 rounded-md bg-indigo-800 text-white text-lg drop-shadow-xl"
+        className="absolute justify-self-center bottom-1/4 z-1 w-60 p-5 rounded-md bg-indigo-800 hover:bg-indigo-600 active:bg-indigo-400 text-white text-lg drop-shadow-xl"
       >
-        ENTER SPACE
+        STEP INTO SPACE
       </button>
     </div>
   );
--- src/components/RocketModel.jsx
+++ src/components/RocketModel.jsx
@@ -12,7 +12,7 @@ export function RocketModel() {
       <ScrollControls pages={1} damping={0.4}>
         <PresentationControls
           snap // Skickar tillbaka modellen till start position oavsett hur man vridit den. 
-           global // - gör att du kan dra vart som helst på hela canvasen, utan den måste du ha musen på modellen. 
+          // global  - gör att du kan dra vart som helst på hela canvasen, utan den måste du ha musen på modellen. 
           zoom={1.25} // högre zoom (2 ex), vid drag - dras modellen närmare dig. 
           rotation={[0, -Math.PI / 3, 0]} 
           polar={[0, Math.PI / 4]} // Bestämmer vertikal begränsning
